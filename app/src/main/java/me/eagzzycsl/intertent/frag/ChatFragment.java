151419e5ee1e3bd9f73afaa5c16b4adcf7b85a09
EAGzzyCSL <eagzzycsl@outlook.com>
2017-05-09 00:11:46 +0800

    [开发:] 消息页面的输入框ui初步

--- app/src/main/java/me/eagzzycsl/intertent/frag/ChatFragment.java
+++ app/src/main/java/me/eagzzycsl/intertent/frag/ChatFragment.java
@@ -8,9 +8,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,9 +32,14 @@ import me.eagzzycsl.intertent.utils.SQLMan;
  * Created by eagzzycsl on 4/17/17.
  */
 
-public class ChatFragment extends Fragment {
+public class ChatFragment extends Fragment implements View.OnClickListener{
     private RecyclerView rec_chat_list;
     private ChatRecAdapter chatRecAdapter;
+    private ImageButton chat_input_more;
+    private ImageButton chat_input_file;
+    private ImageButton chat_input_image;
+    private ImageButton chat_input_send;
+    private EditText chat_input_edit;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -52,6 +61,11 @@ public class ChatFragment extends Fragment {
     }
     private void myFind(View view){
         this.rec_chat_list=(RecyclerView)view.findViewById(R.id.rec_chat_list);
+        this.chat_input_more=(ImageButton)view.findViewById(R.id.chat_input_more);
+        this.chat_input_file=(ImageButton)view.findViewById(R.id.chat_input_file);
+        this.chat_input_image=(ImageButton)view.findViewById(R.id.chat_input_image);
+        this.chat_input_send=(ImageButton)view.findViewById(R.id.chat_input_send);
+        this.chat_input_edit=(EditText)view.findViewById(R.id.chat_input_edit);
 
     }
     private void myCreate(){
@@ -81,5 +95,37 @@ public class ChatFragment extends Fragment {
                 new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false)
         );
         rec_chat_list.setAdapter(chatRecAdapter);
+        chat_input_more.setOnClickListener(this);
+        chat_input_file.setOnClickListener(this);
+        chat_input_image.setOnClickListener(this);
+        chat_input_send.setOnClickListener(this);
+        chat_input_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                toggleSendState(s.length()>0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+        }
+    }
+    private void toggleSendState(boolean send){
+        chat_input_image.setVisibility(send?View.GONE:View.VISIBLE);
+        chat_input_file.setVisibility(send?View.GONE:View.VISIBLE);
+        chat_input_send.setVisibility(send?View.VISIBLE:View.GONE);
     }
 }
